f04bbb1a89995a25e71caea82f98f55975fc71e0
mao <1296193245@qq.com>
2022-10-30 19:58:20 +0800

    AntiSamy_spring_boot_starter_demo

---
+++ tools-xss/src/main/java/mao/tools_xss/ToolsXssApplication.java
@@ -0,0 +1,15 @@
+package mao.tools_xss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsXssApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ToolsXssApplication.class, args);
+    }
+
+}
