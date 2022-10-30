9f1c619059f290844f9d47e5b1c36d1b873044b0
mao <1296193245@qq.com>
2022-10-30 20:31:37 +0800

    AntiSamy_spring_boot_starter_demo

---
+++ tools-xss/src/main/java/mao/tools_xss/config/XssAuthConfiguration.java
@@ -0,0 +1,22 @@
+package mao.tools_xss.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Project name(项目名称)：AntiSamy_spring_boot_starter_demo
+ * Package(包名): mao.tools_xss.config
+ * Class(类名): XssAuthConfiguration
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/10/30
+ * Time(创建时间)： 20:31
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+@Configuration
+public class XssAuthConfiguration
+{
+
+}
